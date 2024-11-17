0a0c129e9224dbf3670d80061600f0cf46d49c75
강나연 <skdusdl4@naver.com>
2024-11-17 17:07:40 +0900

    메인 구성

---
+++ calc-gugudan/main.js
@@ -0,0 +1,22 @@
+// 필요한 모듈을 불러옵니다.
+import readlineSync from 'readline-sync';
+
+// 프로그램 시작
+function main() {
+  // 사용자로부터 입력을 받습니다.
+
+  // 입력에 따라 해당 기능을 실행합니다.
+}
+
+// 사칙연산 계산기 함수
+function calculator() {
+  // 여기에 코드를 작성하세요.
+}
+
+// 구구단 출력 함수
+function printMultiplicationTable() {
+  // 여기에 코드를 작성하세요.
+}
+
+// 프로그램 실행
+main();
