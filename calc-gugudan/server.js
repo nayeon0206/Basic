adb881d34e2b00c3cab826d0f6e8f99cac1f8d76
강나연 <skdusdl4@naver.com>
2024-11-17 17:40:08 +0900

    Update server.js

--- calc-gugudan/server.js
+++ calc-gugudan/server.js
@@ -1,14 +1,10 @@
 // 필요한 모듈을 불러옵니다.
 import readlineSync from 'readline-sync';
-import chalk from 'chalk';
 
 // 프로그램 시작
 function main() {
   // 사용자로부터 입력을 받습니다.
-  console.log(chalk.white(`선택하세요 : `))
-  console.log(chalk.white(`1. 사칙연산 계산기 `))
-  
-  
+
   // 입력에 따라 해당 기능을 실행합니다.
 }
 
