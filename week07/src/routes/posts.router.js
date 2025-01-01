ddfb155d41ef670a258b53a6e390d14ed59a11c1
강나연 <skdusdl4@naver.com>
2025-01-01 22:41:52 +0900

    [add] 베이직반 과제

--- week07/src/routes/posts.router.js
+++ week07/src/routes/posts.router.js
@@ -1,3 +1,28 @@
 // src/routes/posts.router.js
 
 // 숙제입니다.
+
+// 게시물 작성
+router.post("/", (req, res) => {
+    res.send("게시물 작성 엔드포인트");
+  });
+  
+  // 게시물 목록 조회
+  router.get("/", (req, res) => {
+    res.send("게시물 목록 조회 엔드포인트");
+  });
+  
+  // 게시물 상세 조회
+  router.get("/:postId", (req, res) => {
+    res.send("게시물 상세 조회 엔드포인트");
+  });
+  
+  // 게시물 수정
+  router.patch("/:postId", (req, res) => {
+    res.send("게시물 수정 엔드포인트");
+  });
+  
+  // 게시물 삭제
+  router.delete("/:postId", (req, res) => {
+    res.send("게시물 삭제 엔드포인트");
+  });
--- week07/src/routes/user.router.js
+++ week07/src/routes/user.router.js
@@ -5,5 +5,7 @@
 // 유저 정보 조회
 router.get('/users/:userId', authMiddleware, async (req, res) => {
     // 유저 정보 조회 로직
-    // 실제 로직은 작성하지 않습니다. 
+    // 실제 로직은 작성하지 않습니다.
+    const { userId } = req.params;
+  res.send(`프로필 조회: ${userId}`);
   })
