40ca28300fa2a11e79cf45ac2475a7a4cb3d2b92
강나연 <skdusdl4@naver.com>
2025-01-09 16:39:24 +0900

    [add]

--- week08-homework/src/middlewares/validators/create-resume-validator.middleware.js
+++ week08-homework/src/middlewares/validators/create-resume-validator.middleware.js
@@ -2,7 +2,6 @@ import Joi from 'joi';
 import { MESSAGES } from '../../constants/message.constant.js';
 import { MIN_RESUME_LENGTH } from '../../constants/resume.constant.js';
 
-// 조이 오브젝트라는 메서드 이용해서 타이틀과 컨텐트를 검증할 스키마를 만든다.
 const schema = Joi.object({
   title: Joi.string().required().messages({
     'any.required': MESSAGES.RESUMES.COMMON.TITLE.REQUIRED,
@@ -13,7 +12,6 @@ const schema = Joi.object({
   }),
 });
 
-// createResumeValidator 를 진짜로 실행할 수 있는 벨리데이트 함수를 만들었다.
 export const createResumeValidator = async (req, res, next) => {
   try {
     await schema.validateAsync(req.body);
