a7897db8116f0c1286686095b5ece0ff6eadc2d6
강나연 <skdusdl4@naver.com>
2025-01-09 17:36:42 +0900

    [add]

--- week08-homework/src/repositiories/user.repositiory.js
+++ week08-homework/src/repositiories/user.repositiory.js
@@ -2,25 +2,25 @@
 // 2. 서비스
 // 3. 레퍼지토리
 
-import { prisma } from "../utils/prisma.util.js";
+import { prisma } from '../utils/prisma.util.js';
 
 class UserRepositiory {
-    #orm
+  #orm;
 
-    constructor(prisma) {
-        this.#orm = prisma
-    }
+  constructor(prisma) {
+    this.#orm = prisma;
+  }
 
-    signUp = async ({email, password, name }) => {
-        const user = await this.#orm.user.create({
-            data: {
-                email,
-                password,
-                name,
-            },
-        });
-        return user;
-    }
+  signUp = async ({ email, password, name }) => {
+    const user = await this.#orm.user.create({
+      data: {
+        email,
+        password,
+        name,
+      },
+    });
+    return user;
+  };
 }
 
 export default new UserRepositiory(prisma);
